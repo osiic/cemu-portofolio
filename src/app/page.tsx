59d02da2bd295b6d89e91d1b84ad2ef728937a77
Osi ic <osiic.offcl@gmail.com>
2024-07-28 02:22:50 +0700

    depan nya dong

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,103 @@
-import Image from "next/image";
+import { Twitter, Instagram } from "lucide-react"
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <main className="container max-w-screen-sm pt-5">
+        <h1 className="text-4xl font-semibold">cemu</h1>
+        <p className="opacity-80 text-base -mt-1"><span>Software</span> Web Developer</p>
+        <p className="opacity-50 text-sm w-10/12 mt-1">rem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys s</p>
+        <button className="mt-3 font-medium bg-sky-500 hover:bg-white hover:text-sky-900 transition duration-300 rounded-md py-1 px-4 text-base">Resume</button>
+      </main>
+
+      <section className="container max-w-screen-sm pt-10 space-y-4">
+        <h2 className="text-base font-medium">
+          <span># </span>
+          Project
+        </h2>
+
+        <div className="grid gap-x-8 gap-y-5 grid-cols-2">
+
+
+          <div>
+            <div className="overflow-hidden rounded-lg aspect-square bg-gray-800">
+            </div>
+            <h3 className="text-sm font-medium mt-1 line-clamp-1">
+              Mabica Audiobook Website
+            </h3>
+            <p className="text-xs line-clamp-2 font-normal opacity-50">
+              Loremp  kajs kjashd kaj hdl aisu dakj skjd la kjsdh lka
+            </p>
+          </div>
+
+
+          <div>
+            <div className="overflow-hidden rounded-lg aspect-square bg-gray-800">
+            </div>
+            <h3 className="text-sm font-medium mt-1 line-clamp-1">
+              Mabica Audiobook Website
+            </h3>
+            <p className="text-xs line-clamp-2 font-normal opacity-50">
+              Loremp  kajs kjashd kaj hdl aisu dakj skjd la kjsdh lka
+            </p>
+          </div>
+
+          <div>
+            <div className="overflow-hidden rounded-lg aspect-square bg-gray-800">
+            </div>
+            <h3 className="text-sm font-medium mt-1 line-clamp-1">
+              Mabica Audiobook Website
+            </h3>
+            <p className="text-xs line-clamp-2 font-normal opacity-50">
+              Loremp  kajs kjashd kaj hdl aisu dakj skjd la kjsdh lka
+            </p>
+          </div>
+
         </div>
-      </div>
+      </section>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className="container max-w-screen-sm pt-10 space-y-4">
+        <h2 className="text-base font-semibold">
+          <span># </span>
+          Contact
+        </h2>
+        <div className="space-y-2">
+          <div>
+            <h3>
+              E-Mail
+            </h3>
+            <p className="opacity-50">alcemy.hai@gmail.com</p>
+          </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div>
+            <h3>
+              No Phone
+            </h3>
+            <p className="opacity-50">+62 8765 615 283</p>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div>
+            <h3>
+              Social Media
+            </h3>
+            <div className="flex gap-2">
+              <div className="p-2 bg-gray-100/50 hover:bg-gray-100 hover:text-sky-900 transition duration-300	rounded-md ">
+                <Twitter size={20} />
+              </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+              <div className="p-2 bg-gray-100/50 hover:bg-gray-100 hover:text-sky-900 transition duration-300	rounded-md ">
+                <Instagram size={20} />
+              </div>
+            </div>
+          </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        </div>
+      </section>
+
+      <footer className="opacity-50 pb-5 pt-10 text-center">
+        This website was created by <span>cemu</span> with 👍.
+      </footer>
+
+    </>
   );
 }
